90914ca9329219c6bec3c9b568fedcd28eca7188
SaNeOr <534369503@qq.com>
2018-07-31 14:53:36 +0800

    Update main.cpp

--- 02-Hello Triangle/main.cpp
+++ 02-Hello Triangle/main.cpp
@@ -19,16 +19,17 @@ unsigned int indices[] = {
 
 
 const char* vertexShaderSource =
-"#version 330 core										\n	"
-"layout(location = 0) in vec3 aPos;						\n	"		//location :off-set
-"void main(){											\n	"
-"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);}	\n	";
+"#version 330 core                                            \n"
+"layout(location = 0) in vec3 aPos;                           \n"	//location :off-set
+"void main(){                                                 \n"
+"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);}            \n";
+
 
 const char* fragmentShaderSource =
-"#version 330 core									 \n"
-"out vec4 FragColor;								 \n"
-"void main(){										 \n"
-"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);}		 \n";
+"#version 330 core                                  \n"
+"out vec4 FragColor;                                \n"
+"void main(){                                       \n"
+"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);}         \n";
 
 void processInput(GLFWwindow* window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
